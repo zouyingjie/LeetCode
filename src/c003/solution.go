aeea625789decde02b1df43daede1e87fed97d54
zouyingjie <yingjiezou1@gmail.com>
2019-06-24 23:23:18 +0800

    LeetCode003 提交

---
+++ src/c003/solution.go
@@ -0,0 +1,26 @@
+package main
+
+func lengthOfLongestSubstring(s string) int {
+	m := make(map[rune]int)
+	maxLength := 0
+	start := 0
+
+	for index, value := range s {
+
+		if value, ok :=m[value]; ok && value >= start {
+			start = value + 1
+		}
+
+		currentLength := index - start + 1
+		if currentLength > maxLength {
+			maxLength = currentLength
+		}
+		m[value] = index
+	}
+
+	return maxLength
+}
+
+func main() {
+	
+}
