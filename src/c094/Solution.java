44dd92fccc5003b26cc1e9b63f206be9325b290f
zouyingjie <yingjiezou1@gmail.com>
2021-08-02 22:10:21 +0800

    Add c094 solution

---
+++ src/c094/Solution.java
@@ -0,0 +1,26 @@
+package c094;
+
+import com.leetcode.common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    private List<Integer> result = new ArrayList<>();
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        inOrder(root);
+        return result;
+    }
+
+    private void inOrder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+
+        inOrder(node.left);
+        result.add(node.val);
+        inOrder(node.right);
+
+    }
+}
